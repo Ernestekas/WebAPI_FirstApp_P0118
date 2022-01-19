64fb549a22cc530b6caa12c1f6a8d417c2f5ce8f
Ernestekas <ernestekas@gmail.com>
2022-01-19 14:27:15 +0200

    Connection to database established. Models migrations added.

---
+++ SchoolApp/SchoolApp/Migrations/20220119122540_add-school-students-relationship.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SchoolApp.Migrations
+{
+    public partial class addschoolstudentsrelationship : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "SchoolId",
+                table: "Students",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_SchoolId",
+                table: "Students",
+                column: "SchoolId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Students_Schools_SchoolId",
+                table: "Students",
+                column: "SchoolId",
+                principalTable: "Schools",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Students_Schools_SchoolId",
+                table: "Students");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Students_SchoolId",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "SchoolId",
+                table: "Students");
+        }
+    }
+}
--- SchoolApp/SchoolApp/Startup.cs
+++ SchoolApp/SchoolApp/Startup.cs
@@ -2,11 +2,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using SchoolApp.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -28,6 +30,10 @@ namespace SchoolApp
         {
 
             services.AddControllers();
+
+            var defaultConnection = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<DataContext>(d => d.UseSqlServer(defaultConnection));
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "SchoolApp", Version = "v1" });
